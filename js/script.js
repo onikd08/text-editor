0da3007407f42b7f8920496005bd2f789a94a017
Anik Das <onikd08@gmail.com>
2023-10-01 09:41:30 +0300

    Add eventlisteners

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const textAreaField = document.getElementById("text-area");
+
+// Bold
+document.getElementById("bold").addEventListener("click", function () {
+  textAreaField.classList.toggle("font-bold");
+});
+
+// Italic
+document.getElementById("italic").addEventListener("click", function () {
+  textAreaField.classList.toggle("italic");
+});
+
+// Underline
+document.getElementById("underline").addEventListener("click", function () {
+  textAreaField.classList.toggle("underline");
+});
+
+// Left
+document.getElementById("left").addEventListener("click", function () {
+  textAreaField.classList.toggle("text-left");
+});
+
+// Right
+document.getElementById("right").addEventListener("click", function () {
+  textAreaField.classList.toggle("text-right");
+});
+
+//Center
+document.getElementById("center").addEventListener("click", function () {
+  textAreaField.classList.toggle("text-center");
+});
+
+//Justify
+document.getElementById("justify").addEventListener("click", function () {
+  textAreaField.classList.toggle("text-justify");
+});
+
+//font-size
+document.getElementById("font-size").addEventListener("change", function (e) {
+  document.getElementById(
+    "text-area-div"
+  ).style.fontSize = `${e.target.value}px`;
+});
+
+// color
+document.getElementById("color").addEventListener("change", function (e) {
+  document.getElementById("text-area-div").style.color = `${e.target.value}`;
+});
